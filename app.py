153c8377624c9e73a55b160ebde2698e694af1be
luckysingh224422-rgb <luckysingh224422@gmail.com>
2025-10-30 00:43:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ HTML_TEMPLATE = '''
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>AAHAN TOKEN CHECKER - VIP UID Finder</title>
+  <title>AAHAN UID CHECKER - VIP UID Finder</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Rajdhani:wght@300;500;700&display=swap');
